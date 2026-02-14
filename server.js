6af3c759b4ee18152c4aa92c09449b293b9f1b36
ahmedmujtaba39 <ahmedmujtaba.6a@gmail.com>
2026-02-15 00:50:01 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import joinRoutes from "./routes/join.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
+// Routes
+app.use("/api/join", joinRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Student Learning Hub API is running");
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
